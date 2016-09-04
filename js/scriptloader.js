4ae91b5998840796dace66c7a1233966775665d8
Francesco Agnoletto <Francesco.agnoletto92@gmail.com>
2016-09-04 22:21:48 +0200

    .

--- js/scriptloader.js
+++ js/scriptloader.js
@@ -4,7 +4,7 @@ function loadScript(url)
     var script = document.createElement('script');
     script.type = 'text/javascript';
     script.src = url;
-    head.appendChild(script);
+    document.getElementsByTagName("head")[0].appendChild(script);
 }
 loadScript("https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js");
 loadScript("https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js");
