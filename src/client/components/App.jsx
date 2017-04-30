d0c21d66df0912030dd531b2e683f839ace763d9
Robert Pemberton <hellorobertp@gmail.com>
2017-04-30 12:49:16 +0100

    add firebase

--- src/client/components/App.jsx
+++ src/client/components/App.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
+import * as firebase from 'firebase';
 
 import About from './About';
 import Counter from './Counter';
@@ -9,8 +10,20 @@ class App extends Component {
     super();
     this.state = {
       title: 'Hello from React',
+      speed: null,
     };
   }
+
+  componentDidMount() {
+    const rootRef = firebase.database().ref().child('react');
+    const speedRef = rootRef.child('speed');
+    speedRef.on('value', snap => {
+      this.setState({
+        speed: snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div>
@@ -20,6 +33,7 @@ class App extends Component {
           <li><Link to="/about">About</Link></li>
           <li><Link to="/counter">Counter</Link></li>
         </ul>
+        <h2>Speed: {this.state.speed}</h2>
         <Route path="/about" component={About} />
         <Route path="/counter" component={Counter} />
       </div>
--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
 const path = require('path');
-
+const firebase = require('firebase');
 const app = express();
 const port = process.env.PORT || 3000;
 const dist = path.join(__dirname, '../../dist');
 
 app.use(express.static(path.join(__dirname, '../../dist')));
+
+const config = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_API_KEY,
+  databaseURL: process.env.FIREBASE_DB_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+firebase.initializeApp(config);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(dist, 'index.html'));
