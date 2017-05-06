19fa57c4bbb5ee20074cccc1338ff18b7ab280af
Sarah Schneider <rifkegribenes@gmail.com>
2017-05-05 22:58:24 -0700

    added welcome component, small changes to temp nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Link } from 'react-router-dom';
 
 import About from './About';
+import Welcome from './Welcome';
 import ReduxCounter from './ReduxCounter';
 import Firebase from './Firebase';
 import Login from './Login';
@@ -10,22 +11,24 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: 'Hello from Test',
+      title: 'Avis Comunale Rovigo',
     };
   }
 
   render() {
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <ul>
+        <h1 className='temptitle'>{this.state.title}</h1>
+        <ul className='tempnav'>
           <li><Link to="/">Home</Link></li>
+          <li><Link to="/welcome">Welcome</Link></li>
           <li><Link to="/about">About</Link></li>
           <li><Link to="/reduxcounter">ReduxCounter</Link></li>
           <li><Link to="/firebase">Firebase</Link></li>
           <li><Link to="/login">Login</Link></li>
         </ul>
         <hr />
+        <Route path="/welcome" component={Welcome} />
         <Route path="/about" component={About} />
         <Route path="/reduxcounter" component={ReduxCounter} />
         <Route path="/firebase" component={Firebase} />
---
+++ src/components/Welcome.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Welcome = () => (
+  <div>
+   <div className="welcome__image"></div>
+    <h1 className="welcome__text">Diventa donatore</h1>
+    <button className="welcome__button">Scopri Come</button>
+  </div>
+);
+
+export default Welcome;
