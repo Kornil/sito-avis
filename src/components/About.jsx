cabf59639f43831fae89b283019247250625209a
Sarah Schneider <rifkegribenes@gmail.com>
2017-05-06 22:49:58 -0700

    testing max-len

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,9 +3,7 @@ import mySvg from '../images/drops-heart.svg';
 
 const About = () => (
   <div className="about">
-    <div className="about__text">
-            L&rsquo;Avis è un&rsquo;associazione di volontariato con finalità di solidarietà: esclude qualsiasi fine di lucro ed è costituita tra coloro che donano volontariamente, gratuitamente, periodicamente e anonimamente il proprio sangue. È attiva a Rovigo dal 1953 e, secondo i principi della Costituzione, l&rsquo;Avis è apartitica, aconfessionale, senza discriminazione di razza, sesso, religione, lingua, nazionalità né di ideologia politica.
-        </div>
+    <div className="about__text">L&rsquo;Avis è un&rsquo;associazione di volontariato con finalità di solidarietà: esclude qualsiasi fine di lucro ed è costituita tra coloro che donano volontariamente, gratuitamente, periodicamente e anonimamente il proprio sangue. È attiva a Rovigo dal 1953 e, secondo i principi della Costituzione, l&rsquo;Avis è apartitica, aconfessionale, senza discriminazione di razza, sesso, religione, lingua, nazionalità né di ideologia politica.</div>
     <div className="about__ctacontainer">
       <img className="about__image" src={mySvg} alt="" />
       <div className="about__cta">
