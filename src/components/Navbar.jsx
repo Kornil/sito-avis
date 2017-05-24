0d39af9f81364e5a60411e95d45dce77c8d611bf
Sarah Schneider <rifkegribenes@gmail.com>
2017-05-23 23:29:09 -0700

    cleaned up navToggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,58 +4,44 @@ import logo from '../images/AvisRovigoLogo.svg';
 
 class Navbar extends React.Component {
 
-  constructor(props) {
+    constructor(props) {
     super(props);
     this.navToggle = this.navToggle.bind(this);
-    this.navClose = this.navClose.bind(this);
-  }
+    this.homePage = this.homePage.bind(this);
+}
 
-  navToggle() {
-    this.bar1 = document.querySelector('.header__icon__bar-1');
-    this.bar2 = document.querySelector('.header__icon__bar-2');
-    this.bar3 = document.querySelector('.header__icon__bar-3');
-    this.nav = document.querySelector('.header__nav');
-    console.log(this.nav);
-    if (window.innerWidth < 910) {
-      this.bar1.classList.toggle('header__icon__bar-1-active');
-      this.bar2.classList.toggle('header__icon__bar-2-active');
-      this.bar3.classList.toggle('header__icon__bar-3-active');
-      if (!this.nav.classList.contains('header__nav-side')) {
-        this.nav.classList.add('header__nav-side');
-      } else {
-        this.nav.classList.add('header__nav-hidden');
-        setTimeout(() => {
-          this.nav.classList.remove('header__nav-hidden');
-        }, 300);
-        this.nav.classList.remove('header__nav-side');
-      }
+    homePage() {
+        if (this.bar1.classList.contains('header__icon__bar-1-active')) {
+        this.navToggle();
     }
-  }
+}
 
-  navClose() {
-    this.bar1 = document.querySelector('.header__icon__bar-1');
-    this.bar2 = document.querySelector('.header__icon__bar-2');
-    this.bar3 = document.querySelector('.header__icon__bar-3');
-    this.nav = document.querySelector('.nav');
-    if (window.innerWidth < 910) {
-      if (this.bar1.classList.contains('header__icon__bar-1-active')) {
-        this.bar1.classList.remove('header__icon__bar-1-active');
-        this.bar2.classList.remove('header__icon__bar-2-active');
-        this.bar3.classList.remove('header__icon__bar-3-active');
-      }
-      this.nav.classList.add('header__nav-hidden');
-      setTimeout(() => {
-        this.nav.classList.remove('header__nav-hidden');
-      }, 300);
-      this.nav.classList.remove('header__nav-side');
-    }
+    navToggle() {
+        this.bar1 = document.querySelector('.header__icon__bar-1');
+        this.bar2 = document.querySelector('.header__icon__bar-2');
+        this.bar3 = document.querySelector('.header__icon__bar-3');
+        this.nav = document.querySelector('.header__nav');
+          if (window.innerWidth < 910) {
+           this.bar1.classList.toggle('header__icon__bar-1-active');
+           this.bar2.classList.toggle('header__icon__bar-2-active');
+           this.bar3.classList.toggle('header__icon__bar-3-active');
+           if (!this.nav.classList.contains('header__nav-side')) {
+             this.nav.classList.add('header__nav-side');
+           } else {
+             this.nav.classList.add('header__nav-hidden');
+             setTimeout(() => {
+               this.nav.classList.remove('header__nav-hidden');
+             }, 300);
+             this.nav.classList.remove('header__nav-side');
+           }
+        }
   }
 
   render() {
     return (
 
       <header className="header">
-        <Link to="/" onClick={this.navClose} role="link" ><img className="header__image" src={logo} alt="Avis Comunale Rovigo" /></Link>
+        <Link to="/" onClick={this.homePage} role="link" ><img className="header__image" src={logo} alt="Avis Comunale Rovigo" /></Link>
         <button className="header__icon" aria-expanded="false" aria-controls="navbar" onClick={this.navToggle} >
           <span className="sr-only">Toggle navigation</span>
           <div className="header__icon__bar header__icon__bar-1" />
