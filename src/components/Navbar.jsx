82425521809f301d1ac895611391a318e7fe68b3
Sarah Schneider <rifkegribenes@gmail.com>
2017-05-24 18:28:32 -0700

    control nav display with state change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,62 +6,98 @@ class Navbar extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state = {
+      menu: 'closed',
+      width: window.innerWidth,
+    };
     this.navToggle = this.navToggle.bind(this);
     this.homePage = this.homePage.bind(this);
+    this.updateDimensions = this.updateDimensions.bind(this);
+    this.componentWillMount = this.componentWillMount.bind(this);
+    this.componentDidMount = this.componentDidMount.bind(this);
+    this.componentWillUnmount = this.componentWillUnmount.bind(this);
+  }
+
+  componentWillMount() {
+    this.updateDimensions();
+  }
+  componentDidMount() {
+    window.addEventListener('resize', this.updateDimensions);
+  }
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateDimensions);
+  }
+
+  updateDimensions() {
+    this.setState({ width: window.innerWidth });
   }
 
   homePage() {
-    if (this.bar1.classList.contains('header__icon__bar-1-active')) {
+    if (this.state.menu === 'open') {
       this.navToggle();
     }
   }
 
   navToggle() {
-    this.bar1 = document.querySelector('.header__icon__bar-1');
-    this.bar2 = document.querySelector('.header__icon__bar-2');
-    this.bar3 = document.querySelector('.header__icon__bar-3');
-    this.nav = document.querySelector('.header__nav');
-    if (window.innerWidth < 910) {
-      this.bar1.classList.toggle('header__icon__bar-1-active');
-      this.bar2.classList.toggle('header__icon__bar-2-active');
-      this.bar3.classList.toggle('header__icon__bar-3-active');
-      if (!this.nav.classList.contains('header__nav-side')) {
-        this.nav.classList.add('header__nav-side');
+    if (this.state.width < 910) {
+      if (this.state.menu === 'closed') {
+        this.setState({ menu: 'open' });
       } else {
-        this.nav.classList.add('header__nav-hidden');
+        this.setState({ menu: 'closing' });
         setTimeout(() => {
-          this.nav.classList.remove('header__nav-hidden');
+          this.setState({ menu: 'closed' });
         }, 300);
-        this.nav.classList.remove('header__nav-side');
       }
+    } else {
+      this.setState({ menu: 'closed' });
     }
   }
 
   render() {
-    return (
+    const classObj = {
+      closed: {
+        nav: 'header__nav',
+        bar1: 'header__icon__bar header__icon__bar-1',
+        bar2: 'header__icon__bar header__icon__bar-2',
+        bar3: 'header__icon__bar header__icon__bar-3',
+      },
 
+      open: {
+        nav: 'header__nav header__nav-side',
+        bar1: 'header__icon__bar header__icon__bar-1 header__icon__bar-1-active',
+        bar2: 'header__icon__bar header__icon__bar-2 header__icon__bar-2-active',
+        bar3: 'header__icon__bar header__icon__bar-3 header__icon__bar-3-active',
+      },
+
+      closing: {
+        nav: 'header__nav header__nav-hidden',
+        bar1: 'header__icon__bar header__icon__bar-1',
+        bar2: 'header__icon__bar header__icon__bar-2',
+        bar3: 'header__icon__bar header__icon__bar-3',
+      },
+    };
+
+
+    return (
       <header className="header">
         <Link to="/" onClick={this.homePage} role="link" ><img className="header__image" src={logo} alt="Avis Comunale Rovigo" /></Link>
         <button className="header__icon" aria-expanded="false" aria-controls="navbar" onClick={this.navToggle} >
           <span className="sr-only">Toggle navigation</span>
-          <div className="header__icon__bar header__icon__bar-1" />
-          <div className="header__icon__bar header__icon__bar-2" />
-          <div className="header__icon__bar header__icon__bar-3" />
+          <div className={classObj[this.state.menu].bar1} />
+          <div className={classObj[this.state.menu].bar2} />
+          <div className={classObj[this.state.menu].bar3} />
         </button>
-        <nav className="header__nav">
+        <nav className={classObj[this.state.menu].nav}>
           <ul className="header__nav__list">
             <li className="header__nav__item" ><Link to="/associazione" onClick={this.navToggle} role="link">L&rsquo;associazione </Link></li>
             <li className="header__nav__item"><Link to="/faq" onClick={this.navToggle} role="link">FAQ</Link></li>
             <li className="header__nav__item"><Link to="/contatti" onClick={this.navToggle} role="link">Contatti</Link></li>
             <li className="header__nav__item header__nav__item-red"><Link to="/donazione" onClick={this.navToggle} role="link">Donazione</Link></li>
           </ul>
-
         </nav>
       </header>
     );
   }
-
-
 }
 
 export default Navbar;
