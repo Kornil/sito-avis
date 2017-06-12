989d09d37882f7b900917ded4025ede146f732dd
Sarah Schneider <rifkegribenes@gmail.com>
2017-06-11 17:34:26 -0700

    fixing eslint errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,7 +15,6 @@ import Donazione from './Donazione';
 import Login from './Login';
 import CreateBlog from './CreateBlog';
 import UpdateStats from './UpdateStats';
-import ImgUpload from './FileUploadTest';
 import SinglePostDisplay from './SinglePostDisplay';
 
 import { saveAuth } from '../actions';
@@ -57,7 +56,7 @@ class App extends Component {
 
             {this.props.auth && <Route path="/createblog" component={CreateBlog} />}
             {this.props.auth && <Route path="/updatestats" component={UpdateStats} />}
-            {this.props.auth && <Route path="/upload" component={ImgUpload} />}
+
           </Switch>
         </main>
         <Footer />
