4a642c68abafcb1272715a8fd019e60b9723697e
Sarah Schneider <rifkegribenes@gmail.com>
2017-07-30 14:50:13 -0700

    added associazione content

--- src/components/Associazione.jsx
+++ src/components/Associazione.jsx
@@ -1,8 +1,124 @@
 import React from 'react';
+import storia from '../images/storia3.jpg';
 
 const Associazione = () => (
-  <div className="associazione">
-    <div className="associazione__text">Associazione placeholder text.</div>
+  <div className="sp__container">
+    <h2 className="sp__title">Avis Rovigo, dal 1953 solidariet&agrave; e
+     impegno: sempre, ovunque, subito</h2>
+    <div className="sp__img-cont">
+      <img className="sp__img" src={storia} alt="storia" />
+    </div>
+    <h3 className="sp__subhead">Donatori Avis e chi riceve il sangue camminano
+     insieme da pi&ugrave; di 60 anni a Rovigo, con la gratitudine che non ha
+      volto e la consapevolezza che ogni giorno di vita &egrave; un regalo per
+       gli altri e per s&eacute;.</h3>
+    <div className="sp__body">
+      <p>l&rsquo;Avis comunale di Rovigo ha 63 anni e associa 1.605 donatori
+       effettivi al 31 dicembre scorso, che hanno permesso la raccolta di
+        3.184 donazioni nel 2015.</p>
+      <p>Tutto &egrave; iniziato il 4 luglio 1953. &Egrave; la data sull&rsquo;atto
+       costitutivo dell&rsquo;associazione: a sottoscriverlo con l&rsquo;avvocato Beltrame
+        di Monselice, incaricato dall&rsquo;Avis nazionale, furono 29 fondatori: P.
+         Baratella, Pino Barion, don Ostilio Bego, Antonio Bolognesi, Italo
+          Boscolo, Saverio D&rsquo;Amico, Maurizio Fabbri, Pietro Fogagnollo, Paolo
+           Furlanetto, Egidio Gabrielli, Antonio Mercusa, don Gervasio
+            Passadore, Primo Pastorelli, Francesco Pavarin, Antonio Raimondi,
+             Emanuele Raimondi, Giovanni Raisa, Aldo Romagnoli, Dante Schiavo,
+              Giancarlo Stevanin, Antonio Sturaro, Tiziano Tentarelli, Arrigo
+               Vernizzi, Nestore Veronese, Romano Volpato, Pompeo Bacchetti,
+                Giovanni Silvestri, Giovanni Soattin, Lia Soattin. Erano con
+                 loro anche il professor Antonio Borghero, direttore
+                  dell&rsquo;ospedale civile di Rovigo e presidente del Comitato
+                   provinciale, e l&rsquo;avvocato Angelo Schiesari in
+                    rappresentanza dell&rsquo;Avis.</p>
+      <h3 className="sp__subhead">Tutto era gi&agrave; cominciato, per&ograve;
+      , qualche mese prima a Rovigo.</h3>
+      <p>Un gruppo di amici aveva organizzato diversi incontri nei locali
+       della parrocchia di San Francesco: volevano dare aiuto al prossimo
+        donando il sangue. l&rsquo;idea di costituire un&rsquo;associazione aveva riunito
+         Aldo Romagnoli, l&rsquo;avvocato Italo Boscolo, il dottor Paolo Furlanetto,
+          l&rsquo;ingegner Mercusa, la signora Silvana Rossi, il professor Borghero
+           e il dottor Riccardo Morsica, che poi sarebbe stato presidente
+            dell&rsquo;associazione fino al 1960. La prima sede dell&rsquo;Avis di Rovigo
+             fu una stanza di Palazzo Roncale, concessa in affitto dalla Cassa
+              di Risparmio, al canone di 1 lira l&rsquo;anno.</p>
+      <p>Nel 1953, primo anno di attivit&agrave; dell&rsquo;Avis rodigina, i
+       donatori erano 40 e le donazioni furono 13. I  fondatori non restavano
+        fermi nel capoluogo e per far avvicinare all&rsquo;associazione nuovi
+         donatori si adoperavano spostandosi tra  Adria, Badia Polesine,
+          Castelmassa e Ariano.</p>
+      <h3 className="sp__subhead">Cos&igrave; l&rsquo;Avis, mese dopo mese, si
+       diffuse in tutto il Polesine: contava 130 iscritti nel 1955, quando fu
+        possibile, con un contributo dell&rsquo;allora Ministero della Sanit&agrave;
+        , aprire un centro trasfusionale.</h3>
+      <p>Tutto quello che occorreva trov&ograve; posto in una stanza del
+       reparto di Chirurgia dell&rsquo;ospedale civile, allora in via Badaloni: per
+        aprire il centro di raccolta furono acquistati una centrifuga da
+         provette, un lettino per i prelievi, un&rsquo;emoteca, un frigorifero con i
+          flaconi di antisieri per determinare il gruppo sanguigno e l&rsquo;Rh, un
+           tavolo e alcune sedie. Il centro trasfusionale fu spostato poi nel
+            laboratorio di analisi, e infine nel reparto di Medicina, grazie
+             al professor Cavazzuti, che rinunci&ograve; al proprio studio. Il
+              centro trasfusionale era aperto giorno e notte, con turni
+               continuativi di 36 ore del personale infermieristico.</p>
+      <p>Nel 1957 le donazioni (479) erano pi&ugrave; che raddoppiate rispetto
+       a quelle raccolte nel primo anno di apertura del centro (203).</p>
+      <p>Nel 1958 arriv&ograve; l&rsquo;autoemoteca: realizzando un&rsquo;idea del dottor
+       Morsica, raccoglieva le donazioni di paese in paese. l&rsquo;acquisto di un
+        mezzo mobile per la raccolta del sangue fu reso possibile da
+         sottoscrizioni e contributi, offerti dalle banche e dal prefetto
+          Ferruccio Scolaro. Le uscite dell&rsquo;autoemoteca sulle strade polesane
+           diventarono sempre pi&ugrave; frequenti: in particolare dal 1962,
+            quando a dirigere il centro trasfusionale fu chiamato il dottor
+             Ettore Cichella.</p>
+      <p>Sull&rsquo;autoemoteca viaggiavano il primario e un&rsquo;equipe di infermieri:
+       alla guida facevano i turni Aldo Romagnoli, Settimo Esofago e Gino
+        Bolognesi. La raccolta &#x201c;mobile&#x201d; funzionava cos&igrave;.
+         I donatori e il presidente di ogni gruppo locale erano avvisati per
+          lettera dell&rsquo;arrivo dell&rsquo;autoemoteca: i prelievi si facevano ogni
+           sabato pomeriggio e la domenica mattina, i primi anni nelle piazze
+            dei paesi, poi negli ambulatori medici. A volte, l&rsquo;autoemoteca
+             arrivava direttamente a casa dei donatori: succedeva, per
+              esempio, con la famiglia Masiero a Gavello, che contava sei
+               donatori (il padre e cinque figli). Dopo la donazione &#x2013;
+                proprio come accade oggi &#x2013; erano offerti panini, acqua
+                 e bibite.</p>
+      <p>Per avvisare i donatori si usava il telefono sono nelle emergenze:
+       molte chiamate arrivavano di notte e i volontari dell&rsquo;Avis
+        accompagnavano il donatore all&rsquo;ospedale, o in ambulatorio. Una notte
+         capit&ograve; all&rsquo;ospedale civile anche il caso di un pescatore che
+          aveva urgente bisogno di trasfusioni: arriv&ograve; dal Basso
+           Polesine e non era solo. Per aiutarlo, il parroco aveva organizzato
+            un pullman tra i compaesani: quella notte furono raccolte 60
+             donazioni.</p>
+      <p>I donatori rispondevano sempre e subito agli appelli dell&rsquo;allora
+       primario di Chirurgia, il professor Agostino Ferro: riuscivano cos
+       &igrave; a salvare molte vite. Erano ancora gli anni delle trasfusioni
+        dirette, fatte con raccordi a cannula e facendo stendere il ricevente
+         in posizione pi&ugrave; bassa del donatore.</p>
+      <h3 className="sp__subhead">Nel 1962, il numero dei donatori super
+      &ograve; quota 500 e le donazioni la soglia di 2000 (2082).</h3>
+      <p>Con l&rsquo;arrivo dei volontari Avis nelle scuole inizi&ograve; anche la
+       prevenzione del morbo di Cooley: a svolgere i primi screening per la
+        microcitemia fu la professoressa Marta Radici. Poi, oltre alla
+         raccolta del sangue l&rsquo;Avis organizzava feste da ballo e concerti a
+          Rovigo, nei paesi e nei comuni vicini, e partecipava alle
+           manifestazioni delle altre associazioni: erano occasioni per i
+            volontari di promuovere l&rsquo;importanza di donare il sangue e
+             raccogliere le adesioni degli aspiranti donatori.</p>
+      <h3 className="sp__subhead">Gli anni &rsquo;60 furono importantissimi</h3>
+      <p>per le fondamentali scoperte nella immunoematologia, e per la messa a
+       punto di un nuovo protocollo per il trattamento trasfusionale della
+        talassemia, che era molto diffusa in Polesine. Per raggiungere
+         obiettivi di eccellenza, nel 1967 la gestione del centro
+          trasfusionale divent&ograve; una competenza del presidente
+           dell&rsquo;ospedale civile. Fino ad allora era stata l&rsquo;Avis a gestire
+            direttamente il centro donatori: si decise di prendere, come
+             sempre, la decisione migliore, e i volontari del sangue
+              continuarono, anche se in modo diverso, a &#x00ab;essere pronti,
+               sempre pronti, se c&rsquo;&egrave; bisogno.&#x00bb; Sempre, ovunque,
+                subito.</p>
+    </div>
   </div>
 );
 
