e057cb9c78e9d86d7b17ac231d11cbfa25e78d70
Sarah Schneider <rifkegribenes@gmail.com>
2017-09-22 16:36:28 -0700

    added loading spinner to login

---
+++ src/components/Logout.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Logout = () => (
+  <div className="container logout">
+    <div className="logout__header">goodbye</div>
+  </div>
+    );
+
+export default Logout;
