3f0516b2b0b472be0433cb7fa12feca8930ed9d1
Sarah Schneider <rifkegribenes@gmail.com>
2017-10-03 15:27:30 -0700

    split out commmon chunks for main & admin

---
+++ dist/admin.5288ee27.js
@@ -0,0 +1,1 @@
+webpackJsonp([1],{621:function(n,o,p){p(75),p(77),p(125),p(53),p(124),p(76),n.exports=p(126)}},[621]);
--- webpack.config.js
+++ webpack.config.js
@@ -9,12 +9,26 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const APP_DIR = path.resolve(__dirname, 'src');
 const DIST_DIR = path.resolve(__dirname, 'dist');
 
+const isExternal = (module) => {
+  const context = module.context;
+
+  if (typeof context !== 'string') {
+    return false;
+  }
+
+  return context.indexOf('node_modules') !== -1;
+}
+
 const config = {
   entry: {
-     main: APP_DIR + '/index.jsx',
-     vendor: [
-       'react', 'react-dom', 'firebase'
-     ]
+    main: APP_DIR + '/index.jsx',
+    vendors: ['firebase', 'react','react-dom', 'react-router', 'react-router-dom', 'react-redux', 'lodash', 'history'],
+    adminVendors: ['quill', 'react-quill', 'packery', 'react-packery-component', 'react-table', 'react-modal', 'draggabilly', 'react-dropzone'],
+    // createblog: APP_DIR + '/components/CreateBlog.jsx',
+    // modal: APP_DIR + '/components/ModalGuts.jsx',
+    // createphotogallery: APP_DIR + '/components/CreatePhotoGallery.jsx',
+    // galleryindex: APP_DIR + '/components/GalleryIndex.jsx',
+    // blogsindex: APP_DIR + '/components/BlogsIndex.jsx',
    },
   output: {
     path: DIST_DIR,
@@ -55,9 +69,9 @@ const config = {
     new BundleAnalyzerPlugin({
       defaultSizes: 'parsed',
     }),
-    new HtmlWebpackPlugin({
-       title: 'Caching'
-      }),
+    // new HtmlWebpackPlugin({
+    //    title: 'Caching'
+    //   }),
     new webpack.DefinePlugin({
       'process.env': {
         'NODE_ENV': JSON.stringify('production')
@@ -66,11 +80,45 @@ const config = {
     new webpack.optimize.UglifyJsPlugin(),
     new webpack.optimize.AggressiveMergingPlugin(),
     new webpack.optimize.CommonsChunkPlugin({
-       name: 'vendor'
+      name : "adminCommons",
+      chunks: ["adminVendors", "main"],
+      minChunks : 2
      }),
     new webpack.optimize.CommonsChunkPlugin({
-       name: 'runtime'
+      name : "commons",
+      chunks: ["vendors", "main"],
+      minChunks : 2
      }),
+    /* THE CONFIG BELOW (admin-commons, admin-commons-create, admin-commins-index, 'vendors'/'manifest') ALMOST works, but still imports quill into main */
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name : "admin-commons",
+    //   chunks: ["createblog", "modal", "createphotogallery", "galleryindex", "blogsindex"],
+    //   minChunks: 2
+    //  }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "admin-commons-create",
+    //   chunks: ["createblog", "modal", "createphotogallery", "admin-commons"],
+    //   minChunks: 2
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   name: "admin-commons-index",
+    //   chunks: ["galleryindex", "blogsindex", "admin-commons"],
+    //   minChunks: 2
+    // }),
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   names : ['vendors', 'manifest'],
+    //   minChunks: Infinity
+    //  }),
+    // =========== // below makes no difference if added to above four
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   children: true,
+    //   minChunks: 2
+    // }),
+    // //=========== // below also does nothing
+    // new webpack.optimize.CommonsChunkPlugin({
+    //   async: "commonlazy.js",
+    //   children: true
+    // }),
     new CompressionPlugin({
       asset: "[path].gz[query]",
       algorithm: "gzip",
